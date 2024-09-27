bb68e7199fb518091d47152cff5d94f958ac108c
divine_coder <72038689+github2python@users.noreply.github.com>
2024-09-27 14:03:31 +0530

    Update liverSection.js

--- src/components/diseaseSection/liverSection.js
+++ src/components/diseaseSection/liverSection.js
@@ -148,7 +148,7 @@ const DiseaseSection = () => {
     const email = localStorage.getItem("email");
     try {
       const response = await axios.post(
-        `http://localhost:8000/predict/liver/${email}`,
+        `https://healthfizz-backend.onrender.com/predict/liver/${email}`,
         {
           features,
         }
